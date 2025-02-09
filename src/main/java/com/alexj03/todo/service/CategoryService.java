27ee62ed7e3cc8a1641bd84843dd1ff3abe10c09
Alex <feoktistovSano@mail.ru>
2025-02-09 09:36:01 +0300

    feat: added logging in category service

--- src/main/java/com/alexj03/todo/service/CategoryService.java
+++ src/main/java/com/alexj03/todo/service/CategoryService.java
@@ -18,23 +18,28 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     public List<Category> findAll() {
+        log.info("Find all categories");
         return categoryRepository.findAll();
     }
 
     public Category findById(Long id) {
+        log.info("Find category by id: {}", id);
         return categoryRepository.findById(id).orElseThrow(() -> new CategoryNotFoundException(id));
     }
 
     public Category create(CategoryDto categoryDto) {
+        log.info("Create category with title: {}", categoryDto.getTitle());
         Category category = Category.builder().title(categoryDto.getTitle()).build();
         return categoryRepository.save(category);
     }
 
     public void deleteById(Long id) {
+        log.info("Delete category with id: {}", id);
         categoryRepository.deleteById(id);
     }
 
     public Category rename(Long id, CategoryDto categoryDto) {
+        log.info("Rename category by id: {}", id);
         Category category = findById(id);
         category.setTitle(categoryDto.getTitle());
         return categoryRepository.save(category);
